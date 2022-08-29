4b8f90ea491c5f7a5c25c12d66f2dbcc1de9094a
leonardo luz fernandes <leonardo.l.fernandes@gmail.com>
2022-08-29 00:14:41 -0300

    Implementacão utilizando react reducer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React , { useState , useReducer, useRef, useEffect } from 'react';
 import './App.css';
 
-function Todo({ todo , index , completeTodo , removeTodo }){
+function Todo({ todo , index , id , toggleTodo , removeTodo }){
   return (
       <>
         <div 
+          id={`todo-${index}`}
           className='todo' 
-          style={{ textDecoration: todo.isComplete ? "line-through":""}}>
+          style={{ textDecoration: todo.complete ? "line-through":""}}>
           {todo.text}
           <div>
-            <button onClick={() => completeTodo(index)}>Complete</button>
-            <button onClick={() => removeTodo(index)}>X</button>
+            <button onClick={() => toggleTodo(id)}>Complete</button>
+            <button onClick={() => removeTodo(id)}>X</button>
           </div>
         </div>
       </>
@@ -19,19 +20,23 @@ function Todo({ todo , index , completeTodo , removeTodo }){
 
 function TodoForm({addTodo}){
   
-  const [value,setValue] = React.useState(null);
+  const [value,setValue] = useState(null);
+  const inputRef = useRef();
 
   const handleSubmit = ( event ) => {
     event.preventDefault();
-    if(!value) return;
-    addTodo(value);
-    setValue(null);
+    addTodo(inputRef.current.value);
+    inputRef.current.value = '';
+    setValue('');
   };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
         <input 
+          id="add-todo"
+          placeholder='Add Todo...'
+          ref={inputRef}
           type="text"
           className="input"
           value={value}
@@ -44,27 +49,59 @@ function TodoForm({addTodo}){
 
 function App() {
 
-  const [todos,setTodos] = React.useState([
-    {text: "Learn about React" , isComplete: false } ,
-    {text: "Meet friend for lunch" , isComplete: false } ,
-    {text: "Build really cool todo app" , isComplete: false }
-  ])
+  const initialState = [
+    { id: 1 , text: "Learn about React" , complete: false } ,
+    { id: 2 , text: "Meet friend for lunch" , complete: false } ,
+    { id: 3 , text: "Build really cool todo app" , complete: false }
+  ];
+
+  const todoReducer = ( state , action ) => {
+    switch( action.type ){
+      case 'ADD_TODO' : {
+        return ( action.text.length ) ?
+        [...state, {
+          id: state.length ? Math.max(...state.map(todo => todo.id)) + 1 : 0 ,
+          text: action.text ,
+          complete: false , 
+        }] : state ;
+      }
+      case 'TOGGLE_COMPLETE' : {
+        return state.map( ( item ) => 
+          item.id === action.id 
+          ? { ...item , complete: !item.complete }
+          : item
+        )
+      }
+      case 'DELETE_TODO' : {
+        return state.filter( ( item ) => item.id !== action.id );
+      }
+      case 'CLEAR_TODOS' : {
+        return [];
+      }
+      default: {
+        return state;
+      }
+    }
+  };
+
+  const [todos,dispatch] = useReducer( todoReducer , initialState );
+  const completedTodos = todos.filter( todo => todo.complete );
+
+  useEffect(()=>{
+    document.title = `You have ${completedTodos.length} itens completed!` ;
+  });
 
   const addTodo = ( text ) => {
-    const newTodos = [ ...todos , { text } ];
-    setTodos(newTodos);
+    dispatch({ type: 'ADD_TODO' , text });
   };
 
-  const completeTodo = ( index ) => {
-    const newTodos = [...todos] ;
-    newTodos[index].isComplete = true ;
-    setTodos(newTodos);
+  const toggleTodo = ( id ) => {
+    console.log(`Toogle todo with id: ${id}`);
+    dispatch({ type: 'TOGGLE_COMPLETE' , id });
   };
 
-  const removeTodo = ( index ) => {
-    const newTodos = [...todos];
-    newTodos.splice(index,1);
-    setTodos(newTodos);
+  const removeTodo = ( id ) => {
+    dispatch({ type: 'DELETE_TODO' , id });
   }
 
   return (
@@ -72,10 +109,11 @@ function App() {
       <div className='todo-list'>
         {todos.map((todo,index) => (
           <Todo 
-            key={index}
+            key={todo.id}
             index={index}
+            id={todo.id}
             todo={todo} 
-            completeTodo={completeTodo}
+            toggleTodo={toggleTodo}
             removeTodo={removeTodo}/>
         ))}
         <TodoForm addTodo={addTodo}/>
